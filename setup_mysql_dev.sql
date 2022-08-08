fef835e447804d2ca4e7b1db6d6c7d756e62f8a0
Eiden <eidnegabriel07@gmail.com>
2022-08-08 19:01:24 -0400

    fix task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,9 @@
+-- MySQL setup development 
+-- Create database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- Create User
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Grant all privileges to the user on the hbnb_dev_db database
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+--Grant select privileges to the user on database performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
