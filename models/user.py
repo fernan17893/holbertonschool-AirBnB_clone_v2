bcb9e0b10284eec24f2545971119f42a734072cc
fernan17893 <fernan17893@gmail.com>
2022-08-11 11:30:53 -0400

    task 7 erase

--- models/user.py
+++ models/user.py
@@ -9,15 +9,7 @@ from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128))
-        last_name = Column(String(128))
 
-    else:
         email = ''
         password = ''
         first_name = ''
