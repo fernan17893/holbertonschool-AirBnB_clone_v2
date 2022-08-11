2dafcdcdf8890c2b1e4b511460a68ccff58e7d32
Eiden <eidnegabriel07@gmail.com>
2022-08-11 19:32:32 -0400

    task 8

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -38,7 +38,8 @@ class DBStorage:
     def all(self, cls=None):
         """Returns dictionary"""
         table_dict = {}
-        classes = {'State': State, 'City': City, 'User': User, 'Place': Place}
+        classes = {'State': State, 'City': City, 'User': User,
+                   'Place': Place, 'Review': Review, 'Amenity': Amenity}
         if cls is None:
             for c in classes:
                 result = self.__session.query(classes[c]).all()
